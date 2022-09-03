a0a0b39c22a23a92d030c583e12f521ca5b74803
Hannah Brownson <hannahbrownson18@gmail.com>
2022-09-03 18:03:07 -0400

    finished employee js. added the page-template.js to begin gerating the html. not working yet, working on fixing potential bug

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 const Manager = require("./lib/manager");
-// const Intern = require("./lib/intern");
+// const Intern = require("./lib/Assistant");
 // const Engineer = require("./lib/engineer");
 
 // import function that contructs my HTML
@@ -72,3 +72,130 @@ const promptManager = () => {
       employeesArr.push(manager);
     });
 };
+const promptEmployee = (employeesArr) => {
+  //prompts for adding employees
+
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "employeeType",
+        message: "Employee's role? ",
+        choices: ["Engineer", "Assistant"],
+      },
+      {
+        type: "input",
+        name: "name",
+        message: "Employee's name? ",
+        validate: (input) => {
+          if (input) {
+            return true;
+          } else {
+            return false;
+          }
+        },
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "Employee's email? ",
+        validate: (input) => {
+          if (input) {
+            return true;
+          } else {
+            return false;
+          }
+        },
+      },
+      {
+        type: "input",
+        name: "id",
+        message: "Employee's id number? ",
+        validate: (input) => {
+          if (input) {
+            return true;
+          } else {
+            return false;
+          }
+        },
+      },
+      {
+        type: "input",
+        name: "school",
+        message: "What school does the Assistant attend? ",
+        when: ({ employeeType }) => {
+          if (employeeType === "Assistant") {
+            return true;
+          }
+        },
+        validate: (input) => {
+          if (input) {
+            return true;
+          } else {
+            console.log("Please provide a school");
+            return false;
+          }
+        },
+      },
+      {
+        type: "input",
+        name: "github",
+        message: "Employees Github account username? ",
+        when: ({ employeeType }) => {
+          if (employeeType === "Engineer") {
+            return true;
+          }
+        },
+        validate: (input) => {
+          if (input) {
+            return true;
+          } else {
+            console.log("Provide a username");
+            return false;
+          }
+        },
+      },
+      {
+        type: "confirm",
+        name: "confirmMore",
+        message: "Do you have more team members to add? ",
+      },
+      // rerun prompts if there are more team members
+    ])
+    .then((data) => {
+      // add new team memberto array
+      if (data.employeeType === "Engineer") {
+        let engineer = new Engineer(
+          data.name,
+          data.email,
+          data.id,
+          data.github
+        );
+        employeesArr.push(engineer);
+      } else {
+        let Assistant = new Assistant(
+          data.name,
+          data.email,
+          data.id,
+          data.school
+        );
+        employeesArr.push(Assistant);
+      }
+
+      console.log(employeesArr);
+      if (data.confirmMore) {
+        // rerun prompts to verify previous members are save
+        return promptEmployee(employeesArr);
+      } else {
+        // if no more employees are added then return the array
+        return employeesArr;
+      }
+    });
+};
+
+promptManager()
+  .then((employeesArr) => {
+    console.log(employeesArr);
+    return generateHTML(employeesArr);
+  })
+  .then((pageHTML) => {});
